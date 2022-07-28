9236c0ec7814eab6eab74e0b76673ae6967e07a2
jellelubben <83334221+jellelubben@users.noreply.github.com>
2022-07-28 09:49:41 +0200

    Update streamlit_app.py
    changed order

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,9 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
 
@@ -17,4 +20,3 @@ streamlit.dataframe(my_fruit_list)
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 
-my_fruit_list = my_fruit_list.set_index('Fruit')
